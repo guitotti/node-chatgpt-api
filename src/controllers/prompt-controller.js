9a5a8d450dd701347f5ce43ac2d6f7770cb6a250
guilherme_totti <dev.guilhermetotti@gmail.com>
2023-04-30 19:16:01 -0300

    Criação do backend

---
+++ src/controllers/prompt-controller.js
@@ -0,0 +1,25 @@
+const inputPrompt = require("../models/input-prompt");
+const openai = require("../config/openai");
+
+module.exports = {
+    async sendText(req, res){
+        const openaiAPI = openai.configuration();
+        const inputModel = new inputPrompt(req.body);
+
+        try {
+            const response = await openaiAPI.createCompletion(
+                openai.textCompletion(inputModel)
+            )
+
+            return res.status(200).json({
+                success: true,
+                data: response.data.choices[0].text    
+            })
+        } catch (error) {
+            return res.status(400).json({
+                success: false,
+                error: error.response ? error.response : "Oh no! There is an issue on the server."
+            })
+        }
+    }
+}
